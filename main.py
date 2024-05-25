6a14c518725da7cea4a150db699e58868be0ff73
Jayanta Adhikary <jayantaadhikary@protonmail.com>
2024-05-26 01:35:59 +0530

    rag for pdf

---
+++ main.py
@@ -0,0 +1,116 @@
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.vectorstores.chroma import Chroma
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms.ollama import Ollama
+
+DATA_PATH = "data"
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Answer the questions based on the following text:
+{context}
+
+---
+Answer the question based on the above text: {question}
+"""
+
+
+def main():
+    input(
+        "Please make sure you have your pdf files in the 'data' folder and press Enter to continue..."
+    )
+    print("Loading documents...")
+    documents = load_documents()
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
+    your_question = input("What do you want to ask about items in your pdf files?\n")
+    query_rag(your_question)
+
+
+def load_documents():
+    document_loader = PyPDFDirectoryLoader(DATA_PATH)
+    return document_loader.load()
+
+
+def split_documents(documents):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800, chunk_overlap=80, length_function=len, is_separator_regex=False
+    )
+    return text_splitter.split_documents(documents)
+
+
+def get_embedding_function():
+    return OllamaEmbeddings(model="llama3")
+
+
+# create a vector store
+def add_to_chroma(chunks):
+    db = Chroma(
+        persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
+    )
+
+    chunk_with_ids = calculate_chunk_ids(chunks)
+
+    existing_items = db.get(include=[])
+    existing_ids = set(existing_items["ids"])
+    print(f"Existing items: {len(existing_ids)}")
+
+    new_chunks = []
+    for chunk in chunk_with_ids:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+
+    if len(new_chunks):
+        print(f"Adding {len(new_chunks)} new items")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+        db.persist()
+    else:
+        print("No new items to add")
+
+
+def calculate_chunk_ids(chunks):
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        chunk.metadata["id"] = chunk_id
+    return chunks
+
+
+def query_rag(query_text):
+    db = Chroma(
+        persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
+    )
+
+    results = db.similarity_search_with_score(query_text, k=5)
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+
+    model = Ollama(model="llama3")
+    response_text = model.invoke(prompt)
+
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    print(f"Response: {response_text}\nSources: {sources}")
+
+
+
+if __name__ == "__main__":
+    main()
